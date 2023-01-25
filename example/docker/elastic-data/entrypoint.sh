bac94f23979e3ae32767ff34b571fa2e8361f45f
Grzegorz Kołakowski <grzegorz.kolakowski@getindata.com>
2023-01-25 09:40:51 +0100

    Enable security in docker elastic

--- example/docker/elastic-data/entrypoint.sh
+++ example/docker/elastic-data/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-curl -X POST http://elasticsearch:9200/_license/start_trial?acknowledge=true
+curl -u elastic:passwd -X POST http://elasticsearch:9200/_license/start_trial?acknowledge=true
 
 curl -X PUT http://elasticsearch:9200/test_index/ \
+  -u elastic:passwd \
   -H "Content-Type: application/json" \
   --data-binary "@/tmp/test-index.json"
 
 curl -X PUT http://elasticsearch:9200/test_index/_bulk/ \
+  -u elastic:passwd \
   -H "Content-Type: application/json" \
   --data-binary "@/tmp/test-data.json"
